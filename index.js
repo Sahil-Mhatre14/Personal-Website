87fe997cee806537a64ffb24066f6fe2b3baf76d
Sahil-Mhatre14 <sahil81623@gmail.com>
2021-04-21 01:07:25 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+
+// Typed Js
+
+var options = {
+  strings: ['<i>a Web Developer.</i>', '<i>an Android Developer.</i>'],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true
+};
+
+var typed = new Typed('.typing', options);
