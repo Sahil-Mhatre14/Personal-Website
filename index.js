611053010c3a17283fec54dbd250a0e4464fa46b
Sahil-Mhatre14 <sahil81623@gmail.com>
2022-06-02 18:01:44 +0530

    Update skills, projects, etc

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // Typed Js
 
 var options = {
-  strings: ['<i>a Web Developer.</i>', '<i>an Android Developer.</i>'],
+  strings: ['<i>a Web Developer.</i>', '<i>a UI/UX Developer.</i>'],
   typeSpeed: 100,
   backSpeed: 60,
   loop: true
